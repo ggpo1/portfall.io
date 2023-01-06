06d884ee15274aef5c0e3eab8d4a759902af1bbc
v.arkhangelskii <johnlevis199@icloud.com>
2023-01-07 01:57:13 +0300

    feat: mobile browsers height support

--- src/theme/body.ts
+++ src/theme/body.ts
@@ -1,12 +1,24 @@
 import styled from "styled-components";
 
+/** Поддержка мобильных браузеров */
+const resize = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+resize();
+window.addEventListener("resize", resize);
+
 export const Body = styled.div`
-  display: grid;
-  place-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
   height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   background-color: ${({ theme }) => theme.baseColor};
   box-sizing: border-box;
+  overflow: hidden;
 
   * {
     box-sizing: border-box;
