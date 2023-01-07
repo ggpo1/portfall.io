85fe79f4931397fd74cc11ab120e1dba30f6dc50
v.arkhangelskii <johnlevis199@icloud.com>
2023-01-07 03:50:17 +0300

    feat: contacts

--- src/features/contacts/contacts.styles.ts
+++ src/features/contacts/contacts.styles.ts
@@ -5,5 +5,38 @@ export const Container = styled.div`
   width: 100%;
   height: max-content;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+`;
+
+export const Links = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const Link = styled.a`
+  display: flex;
+  gap: 4px;
+  text-decoration: none;
+
+  &:hover {
+    cursor: pointer;
+
+    svg {
+      stroke: ${({ theme }) => theme.backgroundColorActive};
+    }
+  }
+`;
+
+export const LinkIcon = styled.div`
+  width: 16px;
+  height: 16px;
+
+  svg {
+    width: 16px;
+    height: 16px;
+    margin-top: -50%;
+    stroke: #e4e4e4;
+  }
 `;
--- src/features/contacts/contacts.tsx
+++ src/features/contacts/contacts.tsx
@@ -1,10 +1,42 @@
 import { memo } from "react";
+import { Icons } from "assets";
 import * as Markdown from "./contacts.styles";
 
 export const Contacts = memo(() => {
   return (
     <Markdown.Container>
-      Контакты
+      <Markdown.Links>
+        <Markdown.Link href="tel:+&9253744643">
+          +7(925)374-46-43
+          <Markdown.LinkIcon>
+            <Icons.LinkIcon />
+          </Markdown.LinkIcon>
+        </Markdown.Link>
+        <Markdown.Link href="mailto:johnlevis199@gmail.com">
+          johnlevis199@gmail.com
+          <Markdown.LinkIcon>
+            <Icons.LinkIcon />
+          </Markdown.LinkIcon>
+        </Markdown.Link>
+        <Markdown.Link href="https://t.me/varkhn" target="_blank" rel="noopener">
+          Telegram
+          <Markdown.LinkIcon>
+            <Icons.LinkIcon />
+          </Markdown.LinkIcon>
+        </Markdown.Link>
+        <Markdown.Link href="https://github.com/ggpo1" target="_blank" rel="noopener">
+          Github
+          <Markdown.LinkIcon>
+            <Icons.LinkIcon />
+          </Markdown.LinkIcon>
+        </Markdown.Link>
+        <Markdown.Link href="https://stackoverflow.com/users/11835100/vladimir-arkhangelskii" target="_blank" rel="noopener">
+          StackOverflow
+          <Markdown.LinkIcon>
+            <Icons.LinkIcon />
+          </Markdown.LinkIcon>
+        </Markdown.Link>
+      </Markdown.Links>
     </Markdown.Container>
   );
 });
